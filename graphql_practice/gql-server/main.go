14c7d06eebfabf1391eee30863137c32a6f5f24c
terry960302 <terry960302@gmail.com>
2020-04-10 04:43:40 +0900

    etc

--- graphql_practice/gql-server/main.go
+++ graphql_practice/gql-server/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/cmelgarejo/go-gql-server/Config"
 	"github.com/cmelgarejo/go-gql-server/server"
 )
 
 func main() {
 	server.Run()
-	Config.start()
-	Config.UserModel
 }
--- mongo_practice/main.go
+++ mongo_practice/main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
+	ctx        context.Context
 	client     *mongo.Client
 	collection *mongo.Collection
 )
@@ -20,14 +22,15 @@ const (
 
 func main() {
 	initMongo()
+	insertData()
 
 }
 
 func initMongo() {
 
 	var err error
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var cancel func()
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err = mongo.Connect(ctx, options.Client().ApplyURI(
 		"mongodb+srv://terry960302:ljk041180@ritier-joagf.mongodb.net/test?retryWrites=true&w=majority",
@@ -41,4 +44,16 @@ func initMongo() {
 func readCollection() {
 	collection = client.Database(dbName).Collection("User")
 
+}
+
+func insertData() {
+	res, err := collection.InsertOne(ctx, bson.M{"name": "jhj", "id": "kk", "asda": "jnkjk"})
+
+	if err != nil {
+		panic(err)
+	}
+
+	id := res.InsertedID
+
+	println("들어간 데이타 : ", id)
 }
